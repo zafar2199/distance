0bbe9143378d30870516fcb104d061d0f60885bb
zafar2199 <zrozmatov0@gmail.com>
2022-05-17 23:11:06 +1000

    serch input end

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { SearchIcon } from '../../assets/icons'
 
 const Navbar = () => {
-    return <div className='navbar'></div>
+    return (
+        <div className='navbar'>
+            <div className='container'>
+                <div className='navbar__wrap'>
+                    <div className='navbar__left'>
+                        <input
+                            className='navbar__search'
+                            type='text'
+                            placeholder='Search'
+                        />
+                        <button className='navbar__search-btn'>
+                            <SearchIcon />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
